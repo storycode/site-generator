474cec927348458f3c76086e025ed0ab95869d07
Josh Williams <josh@blender.(none)>
2013-10-29 23:56:08 -0400

    better comments

--- import.js
+++ import.js
@@ -24,19 +24,33 @@ sheet.getRows(1, function(err, row_data) {
 
 		var config = docpadInstance.config;
 		var metaCategories = config.templateData.metaCategories;
+		var documentPath = docpadInstance.config.srcPath + '/documents/';
+		var projectsPath = documentPath + 'projects/';
+		var apiPath = documentPath + 'api/';
+		var dataPath = docpadInstance.config.rootPath + '/data/';
 
 		// track related project meta (metaCategories). these turn into top-level sections.
-		// projectMeta.[tools, organizations, people].josh-williams = {
-		//	count: 3,
-		//	slug: josh-williams,
-		//	title: Josh Williams
-		// }
 		var projectMeta = {};
 
-		// take CSV string and return an array of
-		// items with white space trimed from around
-		// each item. If blank, return empty string.
-		var listMaker = function(s){
+		
+		/**
+		 * Take string from Google Doc cell and return an array of items 
+		 * with white space trimed from around each item. 
+		 * If blank, return empty string. Transforms
+		 * string in this form: 'Josh Williams, Meghan Louttit and Tyson Evans'
+		 * to be used in YAML files as:
+		 * 
+		 * people
+		 *	- Josh Williams
+		 *	- Meghan Louttit
+		 *	- Tyson Eans
+		 * 
+		 * Split stings on commas and 'and'
+		 * 
+		 * @param  {String} s Input from spreadseet
+		 * @return {Array/Empty String}
+		 */
+		var yamlArrayMaker = function(s){
 			if (s) {
 				s.trim();
 				return _.flatten(_.map(s.split(','), function(item){
@@ -51,8 +65,26 @@ sheet.getRows(1, function(err, row_data) {
 			}
 		};
 
-		// take project's related meta info (categories, people, etc)
-		// and generate related list of posts for those enrties.
+		
+		/**
+		 * Take a project's related meta info (categories, people, etc) 
+		 * and keep track of slugs found, and number of projects associated
+		 * with the item.
+		 *
+		 * For example, 
+		 *
+		 * // obj.metaCategory.slug.objOfInfo
+		 * 
+		 * projectMeta.[tools, organizations, people].josh-williams.{
+		 *		title: Josh Williams,
+		 *      slug: josh-williams,
+		 *      count: 3, // number of projects related to this object
+		 *      layout: 'person' // layouts/xxx.html.eco to render this asset
+		 * }
+		 * 
+		 * @param  {String} metaCategory people,projects,tools,etc
+		 * @param  {Object} data         { "slug": "people", "singular": "person", "plural": "people" }
+		 */
 		var metaTracker = function(metaCategory, data){
 			if (data) {
 				
@@ -78,13 +110,16 @@ sheet.getRows(1, function(err, row_data) {
 			}
 		};
 
-		// write YAML string to path
+		/**
+		 * Write YAML file to disk
+		 * @param  {String} path    full path
+		 * @param  {String} yamlDoc Fromatted YAML document
+		 */
 		var writeYaml = function(path, yamlDoc){
 			fs.writeFile(path, yamlDoc, function(err) {
 				if (err) {
 					return console.log(err.stack);
 				}
-				// console.log('wrote: ' + path);
 			});
 		};
 
@@ -93,27 +128,36 @@ sheet.getRows(1, function(err, row_data) {
 				if (err) {
 					return console.log(err.stack);
 				}
-				// console.log('wrote: ' + path);
 			});
 		};
 
 
-		// TODO - wrapper around write json, to put files
-		// in documents/api/metacategory/slug.json
-		var writeAPI = function(){};
+		// TODO - 
+		
+		/**
+		 * wrapper around write json, to put filee
+		 * in documents/api/metacategory/slug.json
+		 * @param  {String} metaCategory    people,projects,tools,etc
+		 * @param  {String} slug			slug, like josh-williams
+		 * @param  {Object} jsonObject		relevant data
+		 */
+		var writeAPI = function(metaCategory, slug, jsonObject){
+
+		};
 
 
+		// 
 		// collect meta info on each project
 		var projects = [];
 		row_data.forEach(function(project) {
 			var signatureMaker = crypto.createHash('md5');
 			var meta = {
 				title: project.project.trim(),
-				people: listMaker(project.individuals),
-				organizations: listMaker(project.org),
+				people: yamlArrayMaker(project.individuals),
+				organizations: yamlArrayMaker(project.org),
 				projectUrl: project.url.trim(),
-				categories: listMaker(project.type),
-				tools: listMaker(project.toolsused),
+				categories: yamlArrayMaker(project.type),
+				tools: yamlArrayMaker(project.toolsused),
 				publishedDate: project.published,
 				slug: _.str.slugify( _.str.prune(project.org.trim() + ' ' + project.project.trim(), 70) ), // contact org + project title, limit to 70 chars and slugify
 				country: "United States", // TODO
@@ -135,13 +179,8 @@ sheet.getRows(1, function(err, row_data) {
 		});
 
 		
-
-		// TODO ? -- make sure project doesn't exist and hasn't changedc
-
 		// Generate all project YAML and data files for use in docpad and other scripts
-		var documentPath = docpadInstance.config.srcPath + '/documents/';
-		var projectsPath = documentPath + 'projects/';
-		var dataPath = docpadInstance.config.rootPath + '/data/';
+
 		
 		projects.forEach(function(project) {
 
@@ -155,7 +194,7 @@ sheet.getRows(1, function(err, row_data) {
 			// add to docpad project
 			writeYaml(projectsPath + project.slug + '.html', yamlDoc);
 
-			// write json file
+			// write individual json file for this project
 			writeJSON(dataPath + project.slug + '.json', project);
 		});
 
